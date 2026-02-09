7777be3a914d1cb9e7d96bb33c60f614143d45cd
Theodore Belo <139794007+StellarGrade-Theo@users.noreply.github.com>
2026-02-09 11:29:41 -0500

    injected config service

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service.js';
 import { UsersModule } from './domain/users/users.module.js';
 import { CommonModule } from './common/common.module.js';
 import { EnvModule } from './env/env.module.js';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule, CommonModule, EnvModule],
+  imports: [UsersModule, CommonModule, EnvModule, ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/prisma/prisma.module.ts
+++ src/prisma/prisma.module.ts
@@ -1,8 +1,10 @@
 import { Module, Global } from '@nestjs/common';
 import { PrismaService } from './prisma.service.js';
+import { ConfigModule } from '@nestjs/config';
 
 @Global()
 @Module({
+  imports: [ConfigModule],
   providers: [PrismaService],
   exports: [PrismaService],
 })
