1a17366e66bb45b5a4a6c77752ef34d7a1ac88b5
Theodore Belo <139794007+StellarGrade-Theo@users.noreply.github.com>
2026-02-09 22:31:10 -0500

    basic password hashing, needs to be moved to auth module

--- src/domain/users/users.service.ts
+++ src/domain/users/users.service.ts
@@ -1,17 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto.js';
 import { UpdateUserDto } from './dto/update-user.dto.js';
 import { User } from '../../../generated/prisma/client.js';
 import { PrismaService } from '../../prisma/prisma.service.js';
 import { PaginationDto } from '../../common/dto/pagination.dto.js';
 import { DEFAULT_PAGE_SIZE } from '../../common/util/common.constants.js';
+import { genSalt, hash } from 'bcryptjs';
 
 @Injectable()
 export class UsersService {
   constructor(private prisma: PrismaService) {}
 
   async create(createUserDto: CreateUserDto): Promise<User | null> {
-    return await this.prisma.user.create({ data: createUserDto });
+    const { password } = createUserDto;
+    const hashedPassword = await this.hashPassword(password);
+    const user = await this.prisma.user.create({
+      data: { ...createUserDto, password: hashedPassword },
+    });
+
+    return user;
   }
 
   async findAll(paginationDto: PaginationDto) {
@@ -28,13 +35,24 @@ export class UsersService {
   }
 
   async update(id: number, updateUserDto: UpdateUserDto) {
-    return await this.prisma.user.update({
+    const { password } = updateUserDto;
+    const hashedPassword = password && (await this.hashPassword(password));
+
+    const user = await this.prisma.user.update({
       where: { id },
-      data: updateUserDto,
+      data: { ...updateUserDto, password: hashedPassword },
     });
+    if (!user) throw new NotFoundException('User not found!');
+
+    return user;
   }
 
   async remove(id: number) {
     return await this.prisma.user.delete({ where: { id } });
+  }
+
+  private async hashPassword(password: string) {
+    const salt = await genSalt(12);
+    return hash(password, salt);
   }
 }
