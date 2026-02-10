9b9de3ea00a09e00fadc54219f4715a030cfae0e
Theodore Belo <139794007+StellarGrade-Theo@users.noreply.github.com>
2026-02-10 13:27:15 -0500

    added validation to auth guard

--- src/auth/guards/local-auth/local-auth.guard.ts
+++ src/auth/guards/local-auth/local-auth.guard.ts
@@ -1,9 +1,31 @@
-import { ExecutionContext, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  ExecutionContext,
+  Injectable,
+} from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { LoginDto } from '../../dto/login.dto.js';
+import { plainToClass } from 'class-transformer';
+import { validate } from 'class-validator';
+import { type FastifyRequest } from 'fastify';
 
 @Injectable()
 export class LocalAuthGuard extends AuthGuard('local') {
   async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest<FastifyRequest>();
+    const body = plainToClass(LoginDto, request.body);
+    const errors = await validate(body, {
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    });
+
+    if (errors.length) {
+      const errorMessages = errors
+        .flatMap(({ constraints }) => Object.values(constraints!))
+        .join(', ');
+      throw new BadRequestException(errorMessages);
+    }
+
     const result = (await super.canActivate(context)) as boolean;
 
     return result;
