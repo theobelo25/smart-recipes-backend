3a4c3b213fd60e18263c815f72bdb22f57216413
Theodore Belo <139794007+StellarGrade-Theo@users.noreply.github.com>
2026-02-10 09:09:08 -0500

    added local strategy

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,8 +3,12 @@ import { AuthService } from './auth.service.js';
 import { AuthController } from './auth.controller.js';
 import { HashingService } from './hashing/hashing.service.js';
 import { BcryptService } from './hashing/bcrypt.service.js';
+import { PrismaModule } from '../prisma/prisma.module.js';
+import { PassportModule } from '@nestjs/passport';
+import { LocalStrategy } from './strategies/local.strategy.js';
 
 @Module({
+  imports: [PrismaModule, PassportModule],
   controllers: [AuthController],
   providers: [
     AuthService,
@@ -12,6 +16,7 @@ import { BcryptService } from './hashing/bcrypt.service.js';
       provide: HashingService,
       useClass: BcryptService,
     },
+    LocalStrategy,
   ],
   exports: [HashingService],
 })
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PrismaService } from 'prisma/prisma.service.js';
+import { HashingService } from './hashing/hashing.service.js';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly hashingService: HashingService,
+  ) {}
+
+  async validateLocal(email: string, password: string) {
+    const user = await this.prisma.user.findUnique({
+      select: { id: true, password: true },
+      where: { email },
+    });
+    if (!user) throw new UnauthorizedException('Invalid credentials.');
+
+    const isMatch = await this.hashingService.compare(password, user.password);
+    if (!isMatch) throw new UnauthorizedException('Invalid credentials.');
+
+    return { id: user.id };
+  }
+}
