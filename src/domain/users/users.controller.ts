a6add1451988a39ad18c2e7d19ecd23e1d99a6de
Theodore Belo <139794007+StellarGrade-Theo@users.noreply.github.com>
2026-02-11 14:18:37 -0500

    added rate_limiting

--- src/domain/users/users.controller.ts
+++ src/domain/users/users.controller.ts
@@ -13,12 +13,14 @@ import { CreateUserDto } from './dto/create-user.dto.js';
 import { UpdateUserDto } from './dto/update-user.dto.js';
 import { IdDto } from '../../common/dto/id.dto.js';
 import { PaginationDto } from '../../common/dto/pagination.dto.js';
+import { Public } from '../../auth/decorators/public.decorator.js';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
+  @Public()
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
